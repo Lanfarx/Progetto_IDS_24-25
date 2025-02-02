9e8a68c31f12005110b0969370cfa902de0eb0cc
Lorenzo Marcantognini <lorenz.marcantognini@studenti.unicam.it>
2025-02-02 16:09:26 +0100

    Update RichiestaRuoloController.java

--- FilieraAgricola/src/main/java/it/unicam/cs/filieraagricola/api/controller/RichiestaRuoloController.java
+++ FilieraAgricola/src/main/java/it/unicam/cs/filieraagricola/api/controller/RichiestaRuoloController.java
@@ -13,9 +13,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Optional;
-
 @RestController
 @RequestMapping("/richieste-ruoli")
 public class RichiestaRuoloController {
